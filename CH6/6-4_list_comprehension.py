3ad40d06a7fb0d1ff1ddf261abfbedf427b78ad6
nyan-cat-for-you <164780315+nyan-tech-learn-more@users.noreply.github.com>
2024-04-18 20:39:45 +0800

    Update 6-4_list_comprehension.py

--- CH6/6-4_list_comprehension.py
+++ CH6/6-4_list_comprehension.py
@@ -24,11 +24,10 @@ pass_grades = [g for g in grades if g >= 60 ]
 print(pass_grades)
 #[100, 90, 66, 80, 82, 77]
 
-[g for g in grades if g >= 60] 是一個列表生成式（List Comprehension）的語法。讓我詳細解釋一下：
-
+#[g for g in grades if g >= 60] 是一個列表生成式（List Comprehension）的語法：
 # grades 是一個包含數字的列表，表示學生成績。
 # [g for g in grades if g >= 60] 是一個用來過濾 grades 列表中成績大於等於 60 的元素的表達式。
 # 在這個表達式中，g 是一個臨時變量，它代表 grades 列表中的每個元素。
 # if g >= 60 是一個條件，只有當 g 大於等於 60 時，才會將 g 加入到新的列表中。
 # 因此，[g for g in grades if g >= 60] 會返回一個新的列表，其中包含 grades 中所有大於等於 60 的成績。
-# 在你提供的例子中，pass_grades 就是這個新的列表，其中包含了 [100, 90, 66, 80, 82, 77] 這些成績。這個列表生成式讓我們可以方便地過濾出符合特定條件的元素，並且以簡潔的方式創建新的列表。
+# pass_grades 就是這個新的列表，其中包含了 [100, 90, 66, 80, 82, 77] 這些成績。這個列表生成式可以方便地過濾出符合特定條件的元素，並且以簡潔的方式創建新的列表。
